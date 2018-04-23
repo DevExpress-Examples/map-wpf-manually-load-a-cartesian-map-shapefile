2890e9215ca4b429c48c63a9004cac89b7f33a91
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to manually load a Cartesian map shapefile to a map control
+
+
+This example demonstrates how to load a Cartesian map shapefile to a map control.
+
+
+<h3>Description</h3>
+
+The following predefined coordinate converters are accessed.<br />- AlbersEqualAreaConicCartesianToGeoConverter<br />- LambertConformalConicCartesianToGeoConverter<br />- TransverseMercatorCartesianToGeoConverter<br />- UTMCartesianToGeoConverter&nbsp;<br />- MercatorAuxiliarySphereCartesianToGeoConverter
+
+<br/>
+
+
---
+++ VB/BuildInCoordinateConverters/MainWindow.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports System
+Imports System.Windows
+Imports DevExpress.Xpf.Map
+
+Namespace BuildInCoordinateConverters
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            map.ZoomToFitLayerItems(0.4)
+        End Sub
+    End Class
+
+    Public Class MapData
+        Public Property FileUri() As Uri
+        Public Property CoordinateSystem() As SourceCoordinateSystem
+
+        Public Overrides Function ToString() As String
+            Return String.Format("MapData[FileUri = {0}, CoordinateSystem = {1}]", FileUri, CoordinateSystem)
+        End Function
+    End Class
+End Namespace
