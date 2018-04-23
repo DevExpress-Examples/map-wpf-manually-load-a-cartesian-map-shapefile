e78cb1bdb8ac5c3f56145c66f8d020af3a07c4a7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BuildInCoordinateConverters/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Map;
+
+namespace BuildInCoordinateConverters {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            map.ZoomToFitLayerItems(0.4);
+        }
+    }
+
+    public class MapData {
+        public Uri FileUri { get; set; }
+        public SourceCoordinateSystem CoordinateSystem { get; set; }
+
+        public override string ToString() {
+            return String.Format("MapData[FileUri = {0}, CoordinateSystem = {1}]", FileUri, CoordinateSystem);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to manually load a Cartesian map shapefile to a map control
+
+
+This example demonstrates how to load a Cartesian map shapefile to a map control.
+
+
+<h3>Description</h3>
+
+The following predefined coordinate converters are accessed.<br />- LambertConformalConicCartesianToGeoConverter<br />- TransverseMercatorCartesianToGeoConverter<br />- UTMCartesianToGeoConverter
+
+<br/>
+
+
