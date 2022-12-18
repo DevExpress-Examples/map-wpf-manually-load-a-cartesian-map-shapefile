d3db0ca1f5dbcec4b88bf877a86d47e07eb4346a
DevExpressExampleBot <fakeEmail>
2022-12-18 19:47:24 +0400

    Source auto update [skip ci]

--- VB/BuildInCoordinateConverters/MainWindow.xaml.vb
+++ VB/BuildInCoordinateConverters/MainWindow.xaml.vb
@@ -1,26 +1,29 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.Map
 
 Namespace BuildInCoordinateConverters
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			map.ZoomToFitLayerItems(0.4)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-	Public Class MapData
-		Public Property FileUri() As Uri
-		Public Property CoordinateSystem() As SourceCoordinateSystem
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.map.ZoomToFitLayerItems(0.4)
+        End Sub
+    End Class
 
-		Public Overrides Function ToString() As String
-			Return String.Format("MapData[FileUri = {0}, CoordinateSystem = {1}]", FileUri, CoordinateSystem)
-		End Function
-	End Class
+    Public Class MapData
+
+        Public Property FileUri As Uri
+
+        Public Property CoordinateSystem As SourceCoordinateSystem
+
+        Public Overrides Function ToString() As String
+            Return String.Format("MapData[FileUri = {0}, CoordinateSystem = {1}]", FileUri, CoordinateSystem)
+        End Function
+    End Class
 End Namespace
